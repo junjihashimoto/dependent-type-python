435b59cca384884dbe7d4daa291e2c2c69a750a9
Junji Hashimoto <junji.hashimoto@gmail.com>
2018-01-15 17:24:59 +0900

    Add flatten and reshape

--- ndarray-proto.py
+++ ndarray-proto.py
@@ -1,25 +1,33 @@
 from typing import *
 N = IntVar('N')
 M = IntVar('M')
+N0 = IntVar('N0')
+M0 = IntVar('M0')
+N1 = IntVar('N1')
+M1 = IntVar('M1')
 
 T = TypeVar('T')
 S = TypeVar('S', bound=Shape)
 S0 = TypeVar('S0', bound=Shape)
 S1 = TypeVar('S1', bound=Shape)
+S2 = TypeVar('S2', bound=Shape)
 
 class ndarray(Generic[T,S]):
     def __init__(self):
         pass
 
-class MatMul(Generic[S0,S1]): # This is type level function. It returns fixed shape(a,d) of a result of mutrix multiply(S0(a,b) x S1(c,d)).
-    pass
-
 def add(a: ndarray[T,S],b: ndarray[T,S]) -> ndarray[T,S]:
     pass
 
-def matmul(a: ndarray[T,S0],b: ndarray[T,S1]) -> ndarray[T,MatMul[S0,S1]]:
+def matmul(a: ndarray[T,Shape[N0,M0]],b: ndarray[T,Shape[N1,M1]]) -> ndarray[T,Shape[N0,M1]]:
     pass
-    
+
+def flatten(a: ndarray[T,Shape[N0,M0]]) -> ndarray[T,Shape[N0*M0]]:
+    pass
+
+def reshape(a: ndarray[T,Shape[N0*M0]]) -> ndarray[T,Shape[N0,M0]]:
+    pass
+
 a: ndarray  # just an array, shape and type are arbitrary
 b: ndarray[float, Any]  # array of floats with an unknown (dynamic) shape
 c: ndarray[Any, Shape[100, 100]]  # array of dynamic types with fixed shape (100, 100)
